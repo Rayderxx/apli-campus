2d685e8a6db0a69e8059ae126979210a101b6abf
Amilti <raydersami89@hotmail.com>
2014-09-19 13:53:32 +0200

    init apli-cmapus

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ApliCampus::Application.config.secret_key_base = 'c9bc98922dbcc716e11250b9edf68e3432a1604739046fdf75193c81ed22cfca1522ccd4439a30feb4d33a2b5f763c3dd7b2409b4fab3f62048b0ddd9bcf6377'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ApliCampus::Application.config.session_store :cookie_store, key: '_apli-campus_session'
