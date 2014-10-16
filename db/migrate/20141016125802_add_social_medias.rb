367b460942eb3b7c4de2c6165218d0c9f73b54a3
Rayderxx <sami.errighi@gmail.com>
2014-10-16 21:38:08 +0200

    fix migration

--- db/migrate/20141016125802_add_social_medias.rb
+++
@@ -1,7 +0,0 @@
-class AddSocialMedias < ActiveRecord::Migration
-  def change
-      add_column :information, :facebook, :string
-      add_column :information, :twitter, :string
-      add_column :information, :linkedin, :string
-  end
-end
