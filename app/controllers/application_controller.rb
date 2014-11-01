7771bd09b5f2436d799f52d4d17493be80cad5b1
Rayderxx <sami.errighi@gmail.com>
2014-11-01 18:46:47 +0100

    init prod

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -16,6 +16,6 @@ class ApplicationController < ActionController::Base
     end
 
     def set_rest_client
-        @rest = RestClient::Resource.new('http://localhost:3000/api', :headers => get_header)
+        @rest = RestClient::Resource.new('http://178.62.95.127/api', :headers => get_header)
     end
 end
