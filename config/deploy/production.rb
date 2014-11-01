e37be09a05340522b22de61c187504b985b1c07d
Rayderxx <sami.errighi@gmail.com>
2014-11-01 16:44:30 +0100

    remove capifony

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -4,13 +4,9 @@
 # server in each group is considered to be the first
 # unless any hosts have the primary property set.
 # Don't declare `role :all`, it's a meta role
-role :app, %w{appli-campus@178.62.95.127}
-role :web, %w{appli-campus@178.62.95.127}
-role :db,  %w{appli-campus@178.62.95.127}
-
-set :scm, :git
-set :branch, "feature/amilti"
-set :use_sudo, false
+role :app, %w{deploy@example.com}
+role :web, %w{deploy@example.com}
+role :db,  %w{deploy@example.com}
 
 # Extended Server Syntax
 # ======================
@@ -18,13 +14,7 @@ set :use_sudo, false
 # definition into the server list. The second argument
 # something that quacks like a hash can be used to set
 # extended properties on the server.
-server '178.62.95.127', user: 'appli-campus', roles: %w{web app}, ssh_options: {
-    user: 'appli-campus',
-    keys: %w(/home/user_name/.ssh/id_rsa),
-    forward_agent: false,
-    auth_methods: %w(publickey password),
-    password: 'mqpxgbrx2vv2qqt'
- }
+server 'example.com', user: 'deploy', roles: %w{web app}, my_property: :my_value
 
 # you can set custom ssh options
 # it's possible to pass any option but you need to keep in mind that net/ssh understand limited list of options
